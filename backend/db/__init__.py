ec2ca7f81376414dbff57aba47866c179700ee9d
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-24 14:29:39 -0300

    setup mongodb connection

---
+++ backend/db/__init__.py
@@ -0,0 +1,7 @@
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import config
+
+client = AsyncIOMotorClient(config.MONGODB_URL)
+db = client.get_database("file_to_text")
+files_collection = db.get_collection("files")
---
+++ backend/db/schemas/files.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, ConfigDict, Field
+from db.schemas import PyObjectId
+
+
+class FileModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    name: str = Field()
+    analysis: dict = Field()
+    created_at: datetime = Field(default=datetime.now())
+    is_deleted: bool = Field(default=0)
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "name": "invoice file",
+                "created_at": datetime(2024, 6, 24, 12, 16, 55, 696065),
+                is_deleted: False,
+            }
+        }
+    )
+
+
+class FilesCollection(BaseModel):
+    files: list[FileModel]
