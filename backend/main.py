23c9cd5856673b818aa33e595e5333217e76354b
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-25 14:29:44 -0300

    Update main.py
    - add cors support

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.get_files import get_files_router
 from routes.upload import upload_router
 
@@ -10,13 +11,14 @@ app = FastAPI(
 app.include_router(get_files_router)
 app.include_router(upload_router)
 
-# app.add_middleware(
-#     COMiddleware,
-#     allow_origins=["*"],
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
