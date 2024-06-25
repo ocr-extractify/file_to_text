70d8fc72d0e362f07ef338e2f6a140bfbeffebe4
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-25 09:11:54 -0300

    set DESCRIPTION and convert MAX_FILE_SIZE to MB.

--- frontend/src/constants/uiTexts.ts
+++ frontend/src/constants/uiTexts.ts
@@ -1,8 +1,8 @@
 import { MAX_FILE_SIZE, VALID_MIMETYPES } from "./constraints";
 
 export const TITLE: string = "File to text"
-export const DESCRIPTION: string = "";
+export const DESCRIPTION: string = "Simple app to convert all your files to text.";
 export const FILES_INPUT_LABEL: string = "Files";
 export const UPLOAD_FILE: string = "Upload a file";
 export const OR_DRAG_AND_DROP: string = "or drag and drop";
-export const FILE_INPUT_MIMETYPES: string = `${VALID_MIMETYPES.join(", ")} up to ${MAX_FILE_SIZE.toString()} MB`;
+export const FILE_INPUT_MIMETYPES: string = `${VALID_MIMETYPES.join(", ")} up to ${(MAX_FILE_SIZE / 1024 / 1024).toString()}MB`;
