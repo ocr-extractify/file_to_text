07dc32bd5948f4fb75deafd4b78b1e6be7b8b0cc
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-25 17:12:31 -0300

    Update main.tsx
    - setup react-router-dom and react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { ToastContainer } from 'react-toastify'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import UploadFilesPage from '@/pages/UploadFilesPage.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import '@/main.css';
+import FilesPage from './pages/FilesPage';
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <UploadFilesPage />,
+  },
+  {
+    path: '/files',
+    element: <FilesPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <ToastContainer />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {/* <App /> */}
+      <ToastContainer />
+    </QueryClientProvider>
   </React.StrictMode>,
-)
+);
