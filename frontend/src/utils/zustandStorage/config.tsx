2f125a93006d55de987080a7779a48cf63bbb575
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-26 17:29:58 -0300

    Update config.tsx
    remove setNotUploadedFiles. I'll just store the uploaded files.

--- frontend/src/utils/zustandStorage/config.tsx
+++ frontend/src/utils/zustandStorage/config.tsx
@@ -6,28 +6,17 @@ type State = {
   files: APIFile[];
 };
 
-type Action = {
-  addFiles: (files: APIFile[]) => void;
-  setNotUploadedFiles: (files: APIFile[]) => void;
-};
-
-export const useFilesStore = create<State & Action>(
+export const useFilesStore = create(
   persist(
     (set) => ({
       files: [],
-      setNotUploadedFiles: (files) =>
-        set((state) => {
-          const uploadedFiles = state.files.filter((f) => !f.id);
-          return { files: [...uploadedFiles, ...files] };
-        }),
-      addFiles: (files) =>
-        set((state) => ({ files: [...state.files, ...files] })),
-      // removeFile: (filename) => set((state) => ({ file: file.filter((f) => f !== file) })),
+      add: (file: APIFile) =>
+        set((state: State) => ({ files: [...state.files, file] })),
       removeAll: () => set({ files: [] }),
     }),
     {
-      name: 'files-storage', // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+      name: 'files-storage',
+      storage: createJSONStorage(() => localStorage),
     },
   ),
 );
