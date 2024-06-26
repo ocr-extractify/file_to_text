c3d86f629c11ec6bdef03c5ac5508811158fc4c7
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-26 17:28:53 -0300

    Update FilesPage.tsx

--- frontend/src/pages/FilesPage.tsx
+++ frontend/src/pages/FilesPage.tsx
@@ -1,5 +1,19 @@
+import { APIFile } from '@/utils/types';
+import { useFilesStore } from '@/utils/zustandStorage';
+
 const FilesPage = () => {
-  return <div>FilesPage</div>;
+  const files = useFilesStore((state) => state.files);
+
+  console.log('files loaded', files);
+  return (
+    <div>
+      {files.map((file: APIFile) => (
+        <div key={file.name}>
+          <p>{file.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default FilesPage;
