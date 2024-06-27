836c1fcf22ee7c7bd4d6aab1c45a42080ab7cd3f
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-27 16:03:21 -0300

    Create FilePage.tsx

---
+++ frontend/src/pages/FilePage.tsx
@@ -0,0 +1,22 @@
+import Result from '@/fragments/Result';
+import { httpClient } from '@/utils/axios';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+
+const FilePage = () => {
+  const { id } = useParams<{ id: string }>();
+  const analyzedFile = useQuery({
+    queryKey: ['analyzedFile', id],
+    queryFn: () => httpClient.get(`/files/${id}`),
+  });
+  console.log('AnalyzedFile', analyzedFile);
+
+  return (
+    <Result
+      result={analyzedFile.data?.data}
+      isLoading={analyzedFile.isLoading}
+    />
+  );
+};
+
+export default FilePage;
