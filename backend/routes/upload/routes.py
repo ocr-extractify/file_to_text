8383ec886e1037770c8248d4d0a286f12596ec82
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-27 16:02:09 -0300

    Update routes.py
    - add function to clean the document_ai dict. We remove image (in base64) format from because its too large.

--- backend/routes/upload/routes.py
+++ backend/routes/upload/routes.py
@@ -1,8 +1,9 @@
 from routes.upload import upload_router
 from fastapi import UploadFile, status
-from utils.documentai.analyze import analyze_file
 from db import files_collection
 from db.schemas.files import FileModel
+from utils.documentai.analyze import analyze_file
+from utils.documentai.clean import clean_document_ai_analysis
 
 
 @upload_router.post(
@@ -12,7 +13,9 @@ from db.schemas.files import FileModel
     response_model=FileModel,
 )
 async def upload(file: UploadFile):
-    analyzed_file = {}  # await analyze_file(file)
+    analyzed_file = await analyze_file(file)
+    analyzed_file = await clean_document_ai_analysis(analyzed_file)
+    print("analyzed file")
     file = FileModel(name=file.filename, analysis=analyzed_file)
     file_dict = file.model_dump(by_alias=True, exclude=["id"])
     new_db_file = await files_collection.insert_one(file_dict)
