61887519605eb9f850c24779fb7c7f3a50deb725
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-28 12:15:40 -0300

    Update errorsMsgs.ts

--- frontend/src/constants/errorsMsgs.ts
+++ frontend/src/constants/errorsMsgs.ts
@@ -3,3 +3,4 @@ import { VALID_MIMETYPES } from '@/constants/constraints';
 export const INVALID_MIMETYPE: string = `Filetype not supported. Supported filetypes are ${VALID_MIMETYPES.join(
   ', ',
 )}`;
+export const NO_FILE: string = 'You need to upload at least one file';
