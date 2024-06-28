bb38460efa7c8e32283edfa4129abc609a6fdd74
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-28 12:14:00 -0300

    Create IconButton.tsx

---
+++ frontend/src/components/buttons/IconButton.tsx
@@ -0,0 +1,23 @@
+import React, { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface Props extends ComponentProps<'button'> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const IconButton = ({ children, className, ...rest }: Props) => {
+  return (
+    <button
+      className={twMerge(
+        'p-1.5 rounded-full transition-transform transform active:scale-90 active:bg-gray-300',
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default IconButton;
