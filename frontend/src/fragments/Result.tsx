7ba7156d58a945cdaf54e41133c0bd52a2376087
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-28 09:39:34 -0300

    Update Result.tsx
    - improve grid design

--- frontend/src/fragments/Result.tsx
+++ frontend/src/fragments/Result.tsx
@@ -18,7 +18,7 @@ type Props = {
 const Result = ({ result, isLoading }: Props) => {
   return (
     <div className="relative bg-white">
-      <div className="lg:px-8 lg:items-start">
+      <div className="lg:px-8">
         <div className="relative mx-auto px-4 sm:px-6 lg:px-0 space-y-2">
           <div className="pt-12 sm:pt-16 lg:pt-20 space-y-2">
             {isLoading ? (
@@ -45,7 +45,7 @@ const Result = ({ result, isLoading }: Props) => {
             </TextArea>
           )}
           <div className="mt-10">
-            <dl className="grid grid-cols-2 gap-x-4 gap-y-8">
+            <dl className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8">
               <div className="border-t-2 border-gray-100 pt-6">
                 <dt className="text-2xl font-medium text-gray-500">
                   {ANALYZED_FILE_CONFIDENCE}
@@ -63,18 +63,18 @@ const Result = ({ result, isLoading }: Props) => {
                 </dd>
               </div>
 
-              <div className="border-t-2 border-gray-100 pt-6">
+              <div className="border-t-2 border-gray-100 pt-6 grid">
                 <dt className="text-2xl font-medium text-gray-500">
                   {ANALYZED_FILE_DETECTED_LANGUAGES}
                 </dt>
-                <dd className="text-xl font-medium tracking-tight text-gray-900">
+                <dd className="text-base font-medium tracking-tight text-gray-900">
                   {isLoading ? (
                     <TextSkeleton />
                   ) : (
-                    <div className="space-y-2">
+                    <div className="space-y-2 grid grid-cols-1 sm:grid-cols-2 gap-2">
                       {result.analysis?.pages?.[0]?.detectedLanguages?.map(
                         (dl) => (
-                          <div className="flex flex-col">
+                          <div className="flex flex-col rounded-md shadow-md p-1">
                             <div className="flex justify-between">
                               <span>{ANALYZED_FILE_LANGUAGE_CODE}</span>
                               <span>{dl.languageCode}</span>
