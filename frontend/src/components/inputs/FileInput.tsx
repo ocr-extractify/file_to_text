0f462f135f66f8dd51461b56de6791244b299142
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-28 12:14:10 -0300

    Update FileInput.tsx

--- frontend/src/components/inputs/FileInput.tsx
+++ frontend/src/components/inputs/FileInput.tsx
@@ -2,22 +2,23 @@ import { VALID_MIMETYPES } from '@/constants/constraints';
 import { INVALID_MIMETYPE } from '@/constants/errorsMsgs';
 import {
   FILENAME,
+  FILESIZE,
   FILE_INPUT_MIMETYPES,
   OR_DRAG_AND_DROP,
   UPLOAD_FILE,
 } from '@/constants/uiTexts';
-import { APIFile } from '@/utils/types';
 import { ComponentProps } from 'react';
 import { toast } from 'react-toastify';
+import { IoClose } from 'react-icons/io5';
+import IconButton from '../buttons/IconButton';
 
 interface Props extends ComponentProps<'input'> {
   id: string;
-  files: APIFile[];
-  setFiles: React.Dispatch<React.SetStateAction<[] | APIFile[]>>;
-  isUploading: boolean;
+  files: File[] | [];
+  setFiles: React.Dispatch<React.SetStateAction<[] | File[]>>;
 }
 
-const FileInput = ({ id, files, setFiles, isUploading, ...rest }: Props) => {
+const FileInput = ({ id, files, setFiles, ...rest }: Props) => {
   async function isValidMimetype(mimetype: string): Promise<boolean> {
     if (VALID_MIMETYPES.includes(mimetype)) return true;
     toast.error(INVALID_MIMETYPE);
@@ -39,7 +40,6 @@ const FileInput = ({ id, files, setFiles, isUploading, ...rest }: Props) => {
       const isFile = (file: File | null): file is File => file !== null;
       const validFiles = arrayOfValidFileOrNull.filter(isFile);
       if (validFiles.length > 0) {
-        console.log('valid files', validFiles);
         setFiles((prevState) => [...prevState, ...validFiles]);
       }
     }
@@ -58,7 +58,6 @@ const FileInput = ({ id, files, setFiles, isUploading, ...rest }: Props) => {
     addFiles(event.dataTransfer.files);
   }
 
-  console.log('files', files);
   return (
     <div>
       <label
@@ -66,7 +65,7 @@ const FileInput = ({ id, files, setFiles, isUploading, ...rest }: Props) => {
         onDrop={handleDrop}
         onDragOver={(event) => event.preventDefault()}
       >
-        <div className="w-full h-52 sm:border-t sm:border-gray-200 sm:pt-5">
+        <div className="w-full h-52 sm:pt-5">
           <div className="w-full h-full mt-1 sm:mt-0">
             <div className="h-full flex justify-center items-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
               <div className="space-y-1 text-center">
@@ -107,18 +106,25 @@ const FileInput = ({ id, files, setFiles, isUploading, ...rest }: Props) => {
         {files.map((file) => (
           <li
             key={file.name}
-            className="flex flex-col justify-between border rounded-md p-2 font-medium relative pr-7"
+            className="flex flex-col justify-between border rounded-md p-2 font-medium relative pr-10"
           >
             <span className="w-full flex justify-between">
               <span>{FILENAME}</span>
-              <span>{file.name}</span>
+              <span className="truncate">{file.name}</span>
             </span>
-            <button
+            <span className="w-full flex justify-between">
+              <span>{FILESIZE}</span>
+              <span className="truncate">
+                {(file.size / 1000 / 1000).toFixed(2)} MB
+              </span>
+            </span>
+
+            <IconButton
               onClick={() => removeFile(file.name)}
-              className="absolute right-2 top-2"
+              className="absolute right-2 top-1.5"
             >
-              X
-            </button>
+              <IoClose className="w-5 h-5" />
+            </IconButton>
           </li>
         ))}
       </ul>
