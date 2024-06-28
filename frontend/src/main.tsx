9308657c735cfea19de6b6dbf07fd572d9b6588b
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-28 12:15:53 -0300

    Update main.tsx
    - add base component.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,26 +4,41 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import UploadFilesPage from '@/pages/UploadFilesPage.tsx';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 
 import '@/main.css';
 import FilesPage from './pages/FilesPage';
 import FilePage from './pages/FilePage';
+import Base from './pages/_Base';
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <UploadFilesPage />,
-  },
-  {
-    path: '/files',
-    element: <FilesPage />,
-  },
-  {
-    path: '/files/:id',
-    element: <FilePage />,
+    element: <Base />,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to="/upload" />,
+      },
+      {
+        path: '/upload',
+        element: <UploadFilesPage />,
+      },
+      {
+        path: '/files',
+        element: <FilesPage />,
+      },
+      {
+        path: '/files/:id',
+        element: <FilePage />,
+      },
+    ],
   },
 ]);
 
