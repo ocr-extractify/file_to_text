f179f8ed4d53a933d0a4fba608f7c8fd8cff9ffb
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-28 14:27:36 -0300

    feat: add endpoint to delete a file (/files/{id})

---
+++ backend/routes/files/delete_routes.py
@@ -0,0 +1,19 @@
+from bson import ObjectId
+from routes.files import files_router
+from fastapi import UploadFile, status
+from db import files_collection
+from db.schemas.files import FileModel
+from utils.documentai.analyze import analyze_file
+from utils.documentai.clean import clean_document_ai_analysis
+
+
+@files_router.delete(
+    "/{id}",
+    description="Delete a file by id",
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_model=None,
+)
+async def delete(id: str):
+    doc_to_update = {"_id": ObjectId(id)}
+    data = {"$set": {"is_deleted": True}}
+    files_collection.update_one(doc_to_update, data)
