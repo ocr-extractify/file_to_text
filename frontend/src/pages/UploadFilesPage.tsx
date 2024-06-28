0e39dfca89ea3a884cd52866a700c39c80c7a24f
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-28 12:16:29 -0300

    Update UploadFilesPage.tsx

--- frontend/src/pages/UploadFilesPage.tsx
+++ frontend/src/pages/UploadFilesPage.tsx
@@ -1,19 +1,20 @@
-import { DESCRIPTION, SUBMIT_BUTTON, TITLE } from '@/constants/uiTexts';
+import { SUBMIT_BUTTON } from '@/constants/uiTexts';
 import FileInput from '@/components/inputs/FileInput';
 import { VALID_MIMETYPES } from '@/constants/constraints';
 import { useMutation } from '@tanstack/react-query';
 import { httpClient } from '@/utils/axios';
-import Button from '@/components/Button';
+import Button from '@/components/buttons/Button';
 import { toast } from 'react-toastify';
 import { useFilesStore } from '@/utils/zustandStorage';
 import { useState } from 'react';
 import { APIFile } from '@/utils/types';
 import Result from '@/fragments/Result';
 import { FileStoreState } from '@/utils/zustandStorage/types';
+import { NO_FILE } from '@/constants/errorsMsgs';
 
 function UploadFilesPage() {
   const [results, setResults] = useState<null | APIFile[]>(null);
-  const [files, setFiles] = useState<APIFile[] | []>([]);
+  const [files, setFiles] = useState<File[] | []>([]);
   const addFile = useFilesStore((state: FileStoreState) => state.add);
   const uploadFileMutation = useMutation({
     mutationFn: async (file: File) => {
@@ -36,7 +37,12 @@ function UploadFilesPage() {
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    if (!e.currentTarget.files.files) return;
+    if (e.currentTarget.files.files.length === 0) {
+      toast.error(NO_FILE);
+      return;
+    }
+
+    console.log(e.currentTarget.files.files);
 
     const filesArray: File[] = Array.from(e.currentTarget.files.files);
     Promise.all(
@@ -47,22 +53,22 @@ function UploadFilesPage() {
   }
 
   return (
-    <div className="w-5/6 mx-auto mt-10">
-      <div className="text-center">
-        <h1 className="text-4xl">{TITLE}</h1>
-        <h2 className="text-3xl">{DESCRIPTION}</h2>
-      </div>
+    <div className="mx-auto">
       {!results ? (
         <form className="w-full flex flex-col" onSubmit={handleSubmit}>
           <FileInput
             id="files"
             files={files}
             setFiles={setFiles}
-            isUploading={uploadFileMutation.isPending}
             multiple
             accept={VALID_MIMETYPES.join(',')}
           />
-          <Button className="mt-2 uppercase">{SUBMIT_BUTTON}</Button>
+          <Button
+            className="mt-2 uppercase"
+            isLoading={uploadFileMutation.isPending}
+          >
+            {SUBMIT_BUTTON}
+          </Button>
         </form>
       ) : (
         <>
