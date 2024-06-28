47ec556596d96f82ed4f1707cd2f5fad8d7db290
Vitor Roberto Batista Schirmer <vitor.roberto3022@gmail.com>
2024-06-28 14:33:03 -0300

    feat: improve UI

--- frontend/src/pages/FilesPage.tsx
+++ frontend/src/pages/FilesPage.tsx
@@ -1,14 +1,46 @@
+import IconButton from '@/components/buttons/IconButton';
 import { APIFile } from '@/utils/types';
 import { useFilesStore } from '@/utils/zustandStorage';
+import { RiDeleteBinLine } from 'react-icons/ri';
+import { LiaExternalLinkAltSolid } from 'react-icons/lia';
+import { useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+import { httpClient } from '@/utils/axios';
 
 const FilesPage = () => {
+  const nav = useNavigate();
   const files = useFilesStore((state) => state.files);
+  const removeFile = useFilesStore((state) => state.remove);
+  const deleteMutation = useMutation({
+    mutationFn: async (id: string) => {
+      return httpClient.delete(`/files/${id}`).then(() => {
+        removeFile(id);
+      });
+    },
+  });
 
   return (
-    <div>
+    <div className="space-y-4">
       {files.map((file: APIFile) => (
-        <div key={file._id}>
-          <p>{file.name}</p>
+        <div
+          key={file._id}
+          className="rounded-md p-2 shadow-md shadow-indigo-100 flex justify-between"
+        >
+          <span className="font-medium">{file.name}</span>
+
+          <div>
+            <IconButton
+              onClick={() => {
+                deleteMutation.mutateAsync(file._id);
+              }}
+            >
+              <RiDeleteBinLine className="w-5 h-5" />
+            </IconButton>
+
+            <IconButton onClick={() => nav(`/files/${file._id}`)}>
+              <LiaExternalLinkAltSolid className="w-5 h-5" />
+            </IconButton>
+          </div>
         </div>
       ))}
     </div>
