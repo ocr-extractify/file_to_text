fcae2acd17a04b7925516d8f24dea742845a1634
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-06-30 19:54:06 -0300

    feat: improve <IconButton /> design

--- frontend/src/components/buttons/IconButton.tsx
+++ frontend/src/components/buttons/IconButton.tsx
@@ -10,7 +10,7 @@ const IconButton = ({ children, className, ...rest }: Props) => {
   return (
     <button
       className={twMerge(
-        'p-1.5 rounded-full transition-transform transform active:scale-90 active:bg-gray-300 dark:active:bg-gray-700',
+        'p-1.5 rounded-full hover:text-indigo-400 transition-transform transform active:scale-90 active:text-black active:dark:text-white active:bg-gray-200 dark:active:bg-gray-700',
         className,
       )}
       {...rest}
