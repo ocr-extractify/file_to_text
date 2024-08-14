d73417d93f8393df86e72c86ef0433f53b0b7acd
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-14 16:00:39 -0300

    feat: add routes to get stats by date interval.

--- backend/routes/stats/get_routes.py
+++ backend/routes/stats/get_routes.py
@@ -1,5 +1,6 @@
-from datetime import datetime, timedelta, timezone
-from db.schemas import Stat
+from datetime import datetime, timedelta, timezone, date
+from typing import Union
+from schemas import Stats
 from routes.stats import stats_router
 from fastapi import status
 from db import db
@@ -9,9 +10,9 @@ from db import db
     "/",
     description="Get stats for the uploaded files",
     status_code=status.HTTP_200_OK,
-    response_model=Stat,
+    response_model=Stats,
 )
-async def get_stat():
+async def get_stats():
     files_collection = db.get_collection("files")
 
     uploaded_files_qty = await files_collection.count_documents({})
@@ -32,9 +33,30 @@ async def get_stat():
         {"created_at": {"$gte": last_week}}
     )
 
-    return Stat(
+    return Stats(
         uploaded_files_qty=uploaded_files_qty,
         uploaded_files_qty_month=uploaded_files_qty_month,
         uploaded_files_qty_today=uploaded_files_qty_today,
         uploaded_files_qty_week=uploaded_files_qty_week,
     )
+
+
+@stats_router.get(
+    "/by_date_interval",
+    description="Get the number of uploaded files in a date interval.",
+    status_code=status.HTTP_200_OK,
+    response_model=int,
+)
+async def get_stat(start_date: date, end_date: date | None = None):
+    files_collection = db.get_collection("files")
+
+    if end_date is None:
+        end_date = start_date
+
+    start_datetime = datetime.combine(start_date, datetime.min.time())
+    end_datetime = datetime.combine(end_date, datetime.max.time())
+
+    qty = await files_collection.count_documents(
+        {"created_at": {"$gte": start_datetime, "$lt": end_datetime}}
+    )
+    return qty
