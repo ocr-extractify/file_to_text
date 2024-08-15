f50efe12ccba396b3f989fbee0b40d22cffc7f45
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-15 10:06:08 -0300

    feat: add errors texts.

--- backend/constants/errors_texts.py
+++ backend/constants/errors_texts.py
@@ -1,3 +1,9 @@
 INVALID_FILE_MIMETYPE: str = "Invalid file mimetype."
 INVALID_FILE_ID: str = "Invalid ID format"
 FILE_NOT_FOUND: str = "File does not exist"
+CLIENT_DAY_LIMIT_REACHED: str = (
+    "You have reached the maximum number of uploads for today."
+)
+MONTHLY_LIMIT_REACHED: str = (
+    "The maximum number of uploads for this month has been reached. You'll need to wait until next month to upload again."
+)
