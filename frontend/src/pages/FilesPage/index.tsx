ee31c36db81100388524ad9858543a93d57187c8
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-15 08:41:45 -0300

    feat: improve FilesPage design.

--- frontend/src/pages/FilesPage/index.tsx
+++ frontend/src/pages/FilesPage/index.tsx
@@ -57,7 +57,7 @@ const FilesPage = () => {
       {files.map((file: APIFile) => (
         <div
           key={file._id}
-          className="mt-4 rounded-md p-2 shadow-sm shadow-indigo-100 dark:shadow-indigo-900 flex justify-between dark:text-white"
+          className="flex justify-between bg-white dark:bg-slate-900 mt-4 rounded-md p-2 shadow-sm"
         >
           <span className="font-medium truncate">{file.name}</span>
 
