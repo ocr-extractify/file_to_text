ba2f95df9503f62650815788c5e7916eb053e5d4
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-19 08:59:54 -0300

    fix: fix client_daily_uploads_qty query.

--- backend/routes/files/upload_routes.py
+++ backend/routes/files/upload_routes.py
@@ -31,8 +31,9 @@ async def upload(file: UploadFile, request: Request):
     # check if user day quota is reached
     is_client_restricted = request.client.host not in config.UNRESTRICTED_IPS
     if is_client_restricted:
+        start_of_day = datetime(now.year, now.month, now.day)
         client_daily_uploads_qty = await files_collection.count_documents(
-            {"client_ip": request.client.host}
+            {"client_ip": request.client.host, "created_at": {"$gte": start_of_day}}
         )
         if client_daily_uploads_qty >= config.DAILY_UPLOADS_BY_IP_LIMIT:
             raise HTTPException(status_code=400, detail=CLIENT_DAY_LIMIT_REACHED)
