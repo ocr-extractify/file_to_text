1ad13551a73c3d955016b44bd091d21f217f15d5
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-19 08:58:54 -0300

    feat: add MODE config, indicating the current env.
    inside .env, it should have the key MONGODB_URL_DEV and MONGODB_URL_PROD

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,5 @@
+from typing import Any
+from pydantic import model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
 from dotenv import load_dotenv
@@ -11,11 +13,24 @@ class Config(BaseSettings):
     G_DOCUMENT_AI_PROJECT_ID: str
     G_DOCUMENT_AI_LOCATION: str
     G_DOCUMENT_AI_PROCESSOR: str
-    MONGODB_URL: str
+    MONGODB_URL: str | None = None
+    MONGODB_URL_DEV: str
+    MONGODB_URL_PROD: str
     MONTHLY_UPLOADS_LIMIT: int
     DAILY_UPLOADS_BY_IP_LIMIT: int
     UNRESTRICTED_IPS: str
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
+
+    @model_validator(mode="before")
+    def set_mongodb_attr(cls, values: Any):
+        mode = values.get("MODE")
+
+        if mode == "development":
+            values["MONGODB_URL"] = values.get("MONGODB_URL_DEV")
+        elif mode == "production":
+            values["MONGODB_URL"] = values.get("MONGODB_URL_PROD")
+
+        return values
 
 
 config = Config()
