b35855beea1c84c25c96fa745aed1330d5908628
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-20 12:54:45 -0300

    feat: convert json-string to json-object before write it to disk.

--- backend/utils/documentai/setup_credentials_for_production_env.py
+++ backend/utils/documentai/setup_credentials_for_production_env.py
@@ -6,4 +6,5 @@ from config import config
 async def setup_credentials_for_production_env():
     if config.MODE == "production":
         with open("g_document_ai_creds_personal.json", "w") as f:
-            json.dump(os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON_CONTENT"), f)
+            content = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON_CONTENT")
+            json.dump(json.loads(content), f)
