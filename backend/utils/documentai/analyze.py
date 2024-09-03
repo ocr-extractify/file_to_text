68a00e219f02efbd4758135f09f1eed32250b770
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-09-03 10:53:24 -0300

    wip: trying to setup workload identity federation

--- backend/utils/documentai/analyze.py
+++ backend/utils/documentai/analyze.py
@@ -35,18 +35,54 @@ from google.auth import identity_pool, exceptions, external_account
 #     #     "GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID"
 #     # ),  # Set workforce pool user project.
 # )
+import google.auth
+from google.auth import external_account
+from google.auth.transport import requests
+import os
 
-creds = external_account.Credentials(
-    type="external_account",
-    audience=f"//iam.googleapis.com/projects/${os.getenv('GCP_PROJECT_NUMBER')}/locations/global/workloadIdentityPools/${os.getenv('GCP_WORKLOAD_IDENTITY_POOL_ID')}/providers/${os.getenv('GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID')}",  # Set GCP Audience.
-    token_url="https://sts.googleapis.com/v1/token",  # Set subject token type.
+
+class CustomOIDCCredentials(external_account.Credentials):
+    def retrieve_subject_token(self, request):
+        # This code was developed based in vercel js lib: https://www.npmjs.com/package/@vercel/functions?activeTab=code
+        try:
+            env_token = os.getenv("VERCEL_OIDC_TOKEN")
+            if env_token:
+                return env_token
+
+            response = request(self._token_url)
+            response_headers = response.headers
+            token = response_headers.get("x-vercel-oidc-token")
+            if not token:
+                raise ValueError(
+                    "The 'x-vercel-oidc-token' header is missing from the request. Do you have the OIDC option enabled in the Vercel project settings?"
+                )
+            return token
+
+        except Exception as e:
+            raise exceptions.RefreshError(e, retryable=True)
+
+
+# Instantiate your custom credentials
+creds = CustomOIDCCredentials(
+    audience=f"//iam.googleapis.com/projects/{os.getenv('GCP_PROJECT_NUMBER')}/locations/global/workloadIdentityPools/{os.getenv('GCP_WORKLOAD_IDENTITY_POOL_ID')}/providers/{os.getenv('GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID')}",
     subject_token_type="urn:ietf:params:oauth:token-type:jwt",
-    # subject_token_supplier=supplier,  # Set supplier.
-    service_account_impersonation_url=f"https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${os.getenv('GCP_SERVICE_ACCOUNT_EMAIL')}:generateAccessToken",
-    # workforce_pool_user_project=os.getenv(
-    #     "GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID"
-    # ),  # Set workforce pool user project.
+    token_url="https://sts.googleapis.com/v1/token",
+    service_account_impersonation_url=f"https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/{os.getenv('GCP_SERVICE_ACCOUNT_EMAIL')}:generateAccessToken",
+    credential_source="https://iamcredentials.googleapis.com/v1",
 )
+
+# Use the credentials to authenticate
+request = google.auth.transport.requests.Request()
+creds.refresh(request)
+
+
+# creds = external_account.IdentityPoolCredentials(
+#     audience=f"//iam.googleapis.com/projects/{os.getenv('GCP_PROJECT_NUMBER')}/locations/global/workloadIdentityPools/{os.getenv('GCP_WORKLOAD_IDENTITY_POOL_ID')}/providers/{os.getenv('GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID')}",
+#     subject_token_type="urn:ietf:params:oauth:token-type:jwt",
+#     token_url="https://sts.googleapis.com/v1/token",
+#     subject_token_supplier=supplier,
+#     service_account_impersonation_url=f"https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/{os.getenv('GCP_SERVICE_ACCOUNT_EMAIL')}:generateAccessToken",
+# )
 
 
 async def analyze_file(file: UploadFile):
@@ -65,7 +101,6 @@ async def analyze_file(file: UploadFile):
     # credentials, project = google.auth.default()
     opts = ClientOptions(
         api_endpoint=f"{config.G_DOCUMENT_AI_LOCATION}-documentai.googleapis.com",
-        client_cert_source=credentials,
     )
     client = documentai.DocumentProcessorServiceClient(client_options=opts)
 
--- docs/backend/vercel_and_gcp_integration.md
+++ docs/backend/vercel_and_gcp_integration.md
@@ -1,6 +1,7 @@
 vercel specify how you must integrate gcp with their host in https://vercel.com/docs/security/secure-backend-access/oidc/gcp
 
-read more: 
+references: 
 1. https://cloud.google.com/docs/authentication/provide-credentials-adc
 2. https://googleapis.dev/python/google-auth/2.6.6/reference/google.auth.identity_pool.html
 3. https://cloud.google.com/iam/docs/workload-identity-federation-with-other-clouds
+4. https://vercel.com/changelog/oidc-federation-now-available-in-beta
